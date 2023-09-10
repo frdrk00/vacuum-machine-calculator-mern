cc1053b79e43c1445e3ac9caaaebdffce22d45c0
frdrk00 <masterkooo@gmail.com>
2023-09-10 17:42:45 +0200

    Setup + shadcn

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <div>
+      <Button className="bg-red-300 text-black">Click me</Button>
+    </div>
+  )
+}
+
+export default App
